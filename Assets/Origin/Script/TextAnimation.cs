13fcaa591efefef2bb9b2bc81b277fbf3036703c
kimkiwon <geniikw@gmail.com>
2015-12-19 02:54:56 +0900

    logo example

--- Assets/Origin/Script/TextAnimation.cs
+++ Assets/Origin/Script/TextAnimation.cs
@@ -8,8 +8,27 @@ public class TextAnimation : MonoBehaviour {
     Text m_text;
     Text text { get { return m_text ?? (m_text = GetComponent<Text>()); } }
 
+    float typingSpeed = 0.01f;
+
+      
+    public void Play()
+    {
+        StartCoroutine_Auto(PlayCoroutine());
+    }
+
+    IEnumerator PlayCoroutine()
+    {
+        string end = text.text;
+        text.text = "";
+
+        while(text.text == end)
+        {
+            int n = 0;
+            text.text += end[n];
+            yield return null;
+        }
+
+
+    }
     
-
-
-
 }
